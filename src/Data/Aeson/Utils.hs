1e9c7caa14bb72b8c65393551ccb55f9d5a543e1
Adam Bergmark <adam@bergmark.nl>
2014-01-27 19:30:00 +0100

    aeson-utils: Add eitherDecodeV and decodeV to allow parsing of top level values in a more direct fashion

--- src/Data/Aeson/Utils.hs
+++ src/Data/Aeson/Utils.hs
@@ -3,6 +3,8 @@
 module Data.Aeson.Utils
   ( module Data.Aeson
   , module Data.Aeson.Types
+  , eitherDecodeV
+  , decodeV
   , fromFloatDigits
   , (.=?)
   , withInteger
@@ -15,13 +17,31 @@ module Data.Aeson.Utils
 import Control.Applicative
 import Control.Monad
 import Data.Aeson
+import Data.Aeson.Parser (value)
 import Data.Aeson.Types
-import Data.Text (Text)
 import Data.Scientific
-import qualified Data.HashMap.Strict as H
+import Data.Text            (Text)
+import Data.Attoparsec.Lazy (Result (..))
+import qualified Data.Attoparsec.Lazy as Atto
+import qualified Data.ByteString.Lazy as L
+import qualified Data.HashMap.Strict  as H
 #if ! MIN_VERSION_aeson(0,7,0)
 import Data.Attoparsec.Number
 #endif
+
+-- | Parsing
+
+-- Allows atomic values on the top level
+
+eitherDecodeV :: FromJSON a => L.ByteString -> Either String a
+eitherDecodeV v = case Atto.parse value v of
+  Fail _ _ err -> Left err
+  Done _ r     -> case fromJSON r of
+    Error e   -> Left e
+    Success a -> Right a
+
+decodeV :: FromJSON a => L.ByteString -> Maybe a
+decodeV = either (const Nothing) Just . eitherDecodeV
 
 -- | ToJSON
 
